d0368d0313a29b6d67d43862651bf54e877354f9
omedina-cmu <78521868+omedina-cmu@users.noreply.github.com>
2022-04-29 14:17:47 -0400

    Create insights.m

---
+++ insights.m
@@ -0,0 +1,8 @@
+### ESRI Insights
+
+Insights is an Esri tool to analyze and visualize data that allows you to create workflows and dashboards and dive deep into analytics. It is intuitive, particularly if you have experience with Tableau. The power of Insights is to combine multiple data sets from various sources to perform analysis. 
+
+In my opinion, the best part of Insights is the model builder that maps out your workflow. For a project at work, I visualized survey results. I can export that model, and it can serve as a template to automate the creation of future survey visualization. 
+
+#### Visualizing Survey Data Using Insights
+You can view the Insights page and model here: [https://insights.arcgis.com/#/view/b36eb0865c504a628b725a2354b48fd7](https://insights.arcgis.com/#/view/b36eb0865c504a628b725a2354b48fd7)
